bd3379cdd5c3f8654b58615cf06b05152b8ec0e3
Dylan O'Carroll <dylanocarroll@MyLaptop.local>
2013-04-06 22:26:31 -0700

    Revert "everything but invoke and uniq"
    This reverts commit 20cd057daa64fd52aa947e161df2b40cba748f59.

--- spec/underbarSpec.js
+++ spec/underbarSpec.js
@@ -24,7 +24,7 @@ describe("last", function() {
 
 });
 
-
+/*
 
 describe("first", function() {
   it("should be able to pull out the first element of an array", function() {
@@ -44,7 +44,6 @@ describe("first", function() {
 
 });
 
-
 describe("each", function() {
   it("should provide value and iteration count", function() {
     var letters = ['a', 'b', 'c'];
@@ -61,7 +60,6 @@ describe("each", function() {
     ]);
   });
 });
-
 
 describe("indexOf", function() {
 
@@ -92,7 +90,6 @@ describe("indexOf", function() {
   });
 });
 
-
 describe("filter", function() {
   it("should return all even numbers in an array", function() {
     var isEven = function(num) {
@@ -111,7 +108,6 @@ describe("filter", function() {
   });
 });
 
-
 describe("reject", function() {
   it("should reject all even numbers", function() {
     var isEven = function(num) { return num % 2 === 0; };
@@ -125,7 +121,6 @@ describe("reject", function() {
     expect(evens).to.eql([2, 4, 6]);
   });
 });
-
 
 describe("uniq", function() {
   it("should return all unique values contained in an unsorted array", function() {
@@ -145,14 +140,12 @@ describe("uniq", function() {
   });
 });
 
-
 describe("map", function() {
   it("should apply a function to every value in an array", function() {
     var doubled = _.map([1, 2, 3], function(num) { return num * 2; });
     expect(doubled).to.eql([2, 4, 6]);
   });
 });
-
 
 describe("pluck", function() {
   it("should return values contained at a user-defined property", function() {
@@ -164,7 +157,6 @@ describe("pluck", function() {
     expect(_.pluck(people, 'name')).to.eql(['moe', 'curly']);
   });
 });
-
 
 describe("invoke", function() {
   it("should sort the first array", function() {
@@ -180,7 +172,6 @@ describe("invoke", function() {
   });
 });
 
-/*
 describe("invoke with function reference", function() {
   it("should sort the first array", function() {
     var list = [[5, 1, 7], [3, 2, 1]];
